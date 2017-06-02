eb28339de54c580826d5e56e37b7ab5b538d0b04
Eyassu <utopiaio@yahoo.com>
2017-06-02 16:38:44 +0300

    theme config exported from config 😁

---
+++ app/config/theme.js
@@ -0,0 +1,19 @@
+module.exports = {
+  darkTheme: {
+    background: '#121212',
+    backgroundMuted: '#242424',
+    textMute: '#a0a0a0',
+    textMute2: '#8b8b8b',
+    textDefault: '#ffffff',
+    green: '#2ebd59',
+  },
+
+  lightTheme: {
+    background: '#ffffff',
+    backgroundMuted: '#f7f7f7',
+    textMute: '#4c4c4c',
+    textMute2: '#aeaeae',
+    textDefault: '#4e4e4e',
+    green: '#2ebd59',
+  },
+};

4a2e6fa41d8351e25692d61618e176892b359714
Eyassu <utopiaio@yahoo.com>
2017-06-02 16:48:09 +0300

    on the theme it's background

--- app/components/styled/WolfCola.jsx
+++ app/components/styled/WolfCola.jsx
@@ -12,7 +12,7 @@ const WolfColaContainer = styled.div`
 
 const ControlsContainer = styled.div`
   flex: 0 0 70px;
-  background-color: ${props => props.theme.backgroundColor};
+  background-color: ${props => props.theme.background};
   display: flex;
   flex-direction: row;
 `;
@@ -21,21 +21,21 @@ const NavListContainer = styled.div`
   flex: 1 0 auto;
   display: flex;
   flex-direction: row;
-  background-color: ${props => props.theme.backgroundColor};
+  background-color: ${props => props.theme.background};
 `;
 
 const NavContainer = styled.div`
   flex: 0 0 200px;
   display: flex;
   flex-direction: column;
-  background-color: ${props => props.theme.backgroundColor};
+  background-color: ${props => props.theme.background};
 `;
 
 const ListContainer = styled.div`
   flex: 1 0 80%;
   display: flex;
   flex-direction: column;
-  background-color: ${props => props.theme.backgroundColor};
+  background-color: ${props => props.theme.background};
 `;
 
 module.exports = {

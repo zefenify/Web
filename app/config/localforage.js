88ac693e6f4b50dcfd048b9c405c48adf5bb2f6f
Eyassu <utopiaio@yahoo.com>
2017-06-10 11:25:13 +0300

    lf config

---
+++ app/config/localforage.js
@@ -0,0 +1,18 @@
+import localforage from 'localforage';
+
+const config = {
+  NAME: 'Wolf-Cola',
+  STORE_NAME: 'WOLF_COLA',
+  DESCRIPTION: 'Wolf Cola: The right music player for closure',
+  LF_STORE: {
+    THEME: 'THEME',
+  },
+};
+
+localforage.config({
+  name: config.NAME,
+  storeName: config.STORE_NAME,
+  description: config.DESCRIPTION,
+});
+
+module.exports = config;

11e557b0a551e531688ebac7cac5af83a715273b
Eyassu <utopiaio@yahoo.com>
2017-06-13 15:24:13 +0300

    I know all the states 🤔 ላሽ ላሽ
    - STATE design will frequently change until I sort out the howl event

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,51 +1,3 @@
-/**
- * ### ACTIONS ###
- * PLAY
- * PAUSE
- * NEXT
- * PREVIOUS
- * STOP
- * SEEK
- * PROGRESS
- * REMAINING
- * SHUFFLE
- * CROSSFADE
- * REPEAT
- * LIKE
- * UNLIKE
- * ARTWORK_FULL
- * THEME
- * SEARCH
- * LOADING
- * ONLINE
- * SAVE
- * REMOVE
- * CREATE_PLAYLIST
- * UPDATE_PLAYLIST
- * DELETE_PLAYLIST
- * ADD_TO_PLAYLIST
- * REMOVE_FROM_PLAYLIST
- *
- * ### STATE ###
- * {
- *   theme: 'light | dark',
- *   queue: [],
- *   history: [],
- *   match: [],
- *   saved: [],
- *   playlist: [],
- *   crossfade: 0, // MAX: 12
- *   remaining: Boolean, // false -> song length, true -> -remaining
- *   artworkFull: Boolean,
- *   playing: Boolean,
- *   shuffle: Boolean,
- *   repeat: 'OFF|ONE|ALL',
- *   current: null | Object, // state update 1 second interval, reselect at bay
- *   online: Boolean, // even tho adding offline capability is possible, I will *NOT* do that
- *   loading: Boolean,
- * }
- */
-
 /* global window */
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
@@ -54,6 +6,7 @@ import theme from 'app/redux/reducer/theme';
 import volume from 'app/redux/reducer/volume';
 import repeat from 'app/redux/reducer/repeat';
 import shuffle from 'app/redux/reducer/shuffle';
+
 import rootSaga from 'app/redux/saga/sagas';
 
 const sagaMiddleware = createSagaMiddleware();

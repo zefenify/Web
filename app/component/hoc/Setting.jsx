782ab4923a25e47fbef89dd76d2a60e2e1f5b49a
Eyassu <utopiaio@yahoo.com>
2017-06-14 18:20:51 +0300

    crossfade

--- app/component/hoc/Setting.jsx
+++ app/component/hoc/Setting.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { func, string } from 'prop-types';
+import { func, string, number } from 'prop-types';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
 import { SET_THEME } from 'app/redux/constant/theme';
+import { SET_CROSSFADE } from 'app/redux/constant/crossfade';
 
 import Button from 'app/component/styled/Button';
 import Divider from 'app/component/styled/Divider';
+import Range from 'app/component/styled/Range';
 
 const SettingContainer = styled.div`
   flex: 1 1 auto;
@@ -14,9 +16,19 @@ const SettingContainer = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 1em;
+
+  .crossfade {
+    padding: 0;
+    width: 75%;
+    max-width: 250px;
+    margin-top: 2em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
-const Setting = ({ currentTheme, toggleTheme }) => (
+const Setting = ({ currentTheme, currentCrossfade, crossfade, toggleTheme }) => (
   <SettingContainer>
     <h1>Settings</h1>
 
@@ -27,22 +39,42 @@ const Setting = ({ currentTheme, toggleTheme }) => (
     <small>
       <span>Current theme is </span><b>{ currentTheme }</b>
     </small>
+
+    <div className="crossfade">
+      <h3>Crossfade: { currentCrossfade === 0 ? 'Off' : `${currentCrossfade} Second${currentCrossfade > 1 ? 's' : ''}` }</h3>
+
+      <Range
+        type="range"
+        onChange={e => crossfade(e)}
+        value={currentCrossfade}
+        min="0"
+        max="12"
+        step="1"
+      />
+    </div>
   </SettingContainer>
 );
 
 Setting.propTypes = {
   currentTheme: string,
+  currentCrossfade: number,
   toggleTheme: func.isRequired,
+  crossfade: func.isRequired,
 };
 
 Setting.defaultProps = {
   currentTheme: '',
+  currentCrossfade: 0,
 };
 
 module.exports = connect(state => ({
   currentTheme: state.theme,
+  currentCrossfade: state.crossfade,
 }), dispatch => ({
   toggleTheme() {
     dispatch({ type: SET_THEME });
+  },
+  crossfade(e) {
+    dispatch({ type: SET_CROSSFADE, payload: Number.parseInt(e.target.value, 10) });
   },
 }))(Setting);

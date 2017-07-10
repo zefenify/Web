ec180fa1b762e539dee648160ee0d9a918c7eb0f
Eyassu <utopiaio@yahoo.com>
2017-07-10 19:00:11 +0300

    dumbo mode
    - on dumbo mode returns an object { hours, minutes, seconds }

--- app/util/time.js
+++ app/util/time.js
@@ -3,9 +3,10 @@ module.exports = {
    * given duration in seconds returns hour:minute:second
    *
    * @param {Number} duration `Howl.duration` (seconds)
+   * @param {Boolean} dumbo
    * @type {String}
    */
-  human(duration = 0) {
+  human(duration = 0, dumbo = false) {
     const parsedDuration = Number.parseFloat(duration);
 
     // this will be triggered on next and remainder is true
@@ -17,6 +18,14 @@ module.exports = {
     const minutes = Math.floor((parsedDuration - (hours * 3600)) / 60);
     const seconds = Math.floor(parsedDuration - ((hours * 3600) + (minutes * 60)));
 
-    return `${hours > 0 ? hours : ''}${hours > 0 ? ':' : ''}${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    if (dumbo === false) {
+      return `${hours > 0 ? hours : ''}${hours > 0 ? ':' : ''}${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    }
+
+    return {
+      hours,
+      minutes,
+      seconds,
+    };
   },
 };

fa07d4941adb216021504b7bf1aadf15e942b4c2
Eyassu <utopiaio@yahoo.com>
2017-07-10 09:41:34 +0300

    breaking Saga on API caller
    Excuse:
    - too much boilerplate for a single entry point - I'm ready to shoot myself in the foot
    
    - removed logs
    - loading now triggered with howler loading and API loading

--- app/util/api.js
+++ app/util/api.js
@@ -6,23 +6,27 @@
  * through out the lifetime...still `N` 4:44 SICK BEAT!
  */
 
+// Breaking SAGA! 😔
+import store from '@app/redux/store';
+import { loading } from '@app/redux/action/loading';
+
 const API_CACHE = {};
 
 module.exports = URL => new Promise((resolve, reject) => {
   if (Object.prototype.hasOwnProperty.call(API_CACHE, URL)) {
-    console.log(`returning from cache [${URL}]`);
     resolve(Object.assign(Array.isArray(API_CACHE[URL]) ? [] : {}, API_CACHE[URL]));
     return;
   }
 
-  console.log(`fetching for [${URL}]...`);
+  store.dispatch(loading(true));
   fetch(URL)
     .then(response => response.json())
     .then((data) => {
+      store.dispatch(loading(false));
       API_CACHE[URL] = data;
-      console.log(`saved [${URL}] to cache`);
       resolve(Object.assign(Array.isArray(API_CACHE[URL]) ? [] : {}, API_CACHE[URL]));
     }, (err) => {
+      store.dispatch(loading(false));
       reject(err);
     });
 });

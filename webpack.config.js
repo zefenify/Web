e9594fa7cb3b67994dbfd08c83b5fdd788d10807
Eyassu <utopiaio@yahoo.com>
2017-07-14 12:31:04 +0300

    better `env` passing

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const PRODUCTION = process.env.NODE_ENV === 'production';
-
 const commonPlugins = [
   new ExtractTextPlugin({
     filename: '[name].bundle.css',
@@ -17,112 +15,116 @@ const commonPlugins = [
   new webpack.optimize.ModuleConcatenationPlugin(),
 ];
 
-module.exports = {
-  entry: {
-    vendor: [
-      'localforage',
-      'react',
-      'react-dom',
-      'react-redux',
-      'react-router',
-      'react-router-dom',
-      'redux',
-      'redux-saga',
-      'reselect',
-      'emotion',
-    ],
-    app: ['./app/index.jsx'],
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.join(__dirname, 'build'),
-    publicPath: '/build/',
-  },
-  resolve: {
-    alias: { '@app': path.resolve(__dirname, 'app/') },
-    extensions: ['.js', '.jsx', '.scss'],
-  },
-  module: {
-    rules: [
-      // js[x]
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          plugins: [
-            'emotion/babel',
-          ],
-          presets: [
-            'react',
-            ['env', { targets: { browsers: ['safari >= 10'] } }], // 100% ES2015
-          ],
+module.exports = (env) => {
+  const PRODUCTION = env === 'production';
+
+  return {
+    entry: {
+      vendor: [
+        'localforage',
+        'react',
+        'react-dom',
+        'react-redux',
+        'react-router',
+        'react-router-dom',
+        'redux',
+        'redux-saga',
+        'reselect',
+        'emotion',
+      ],
+      app: ['./app/index.jsx'],
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.join(__dirname, 'build'),
+      publicPath: '/build/',
+    },
+    resolve: {
+      alias: { '@app': path.resolve(__dirname, 'app/') },
+      extensions: ['.js', '.jsx', '.scss'],
+    },
+    module: {
+      rules: [
+        // js[x]
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          options: {
+            plugins: [
+              'emotion/babel',
+            ],
+            presets: [
+              'react',
+              ['env', { targets: { browsers: ['safari >= 10'] } }], // 100% ES2015
+            ],
+          },
         },
-      },
 
-      // css
-      {
-        test: /\.css$/,
-        exclude: /emotion\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: {
-            loader: 'css-loader',
-            options: {
-              sourceMap: PRODUCTION,
-              modules: true,
+        // css
+        {
+          test: /\.css$/,
+          exclude: /emotion\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: {
+              loader: 'css-loader',
+              options: {
+                sourceMap: PRODUCTION,
+                modules: true,
+              },
             },
-          },
-        }),
-      },
+          }),
+        },
 
-      // emotion.css
-      {
-        test: /emotion\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: {
-            loader: 'css-loader',
-            options: {
-              sourceMap: PRODUCTION,
+        // emotion.css
+        {
+          test: /emotion\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: {
+              loader: 'css-loader',
+              options: {
+                sourceMap: PRODUCTION,
+              },
             },
-          },
-        }),
-      },
+          }),
+        },
 
-      // fonts
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader?name=static/[name].[ext]',
-      },
+        // fonts
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'file-loader?name=static/[name].[ext]',
+        },
 
-      // sass
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'postcss-loader', 'sass-loader'],
-        }),
-      },
-    ],
-  },
-  devtool: PRODUCTION ? 'source-map' : 'eval',
-  devServer: {
-    publicPath: 'http://localhost:8080/build/',
-  },
-  plugins: PRODUCTION ? [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false,
-      },
-    }),
-  ].concat(commonPlugins) : [
-    // add development plugins here
-  ].concat(commonPlugins),
+        // sass
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'postcss-loader', 'sass-loader'],
+          }),
+        },
+      ],
+    },
+    devtool: PRODUCTION ? 'source-map' : 'eval',
+    devServer: {
+      publicPath: 'http://localhost:8080/build/',
+    },
+    plugins: PRODUCTION ? [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production'),
+        },
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        compress: {
+          warnings: false,
+        },
+      }),
+    ].concat(commonPlugins) : [
+      // add development plugins here
+    ].concat(commonPlugins),
+  };
 };

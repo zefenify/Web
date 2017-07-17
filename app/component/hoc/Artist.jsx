3d4a3bb6040fe32a2e7f6064d3a0b4b401fea0ee
Eyassu <utopiaio@yahoo.com>
2017-07-17 06:14:51 +0300

    ማ ነው
    - ማ ነው fer kedjag
    - dispatches connected

--- app/component/hoc/Artist.jsx
+++ app/component/hoc/Artist.jsx
@@ -124,6 +124,20 @@ const ArtistContainer = styled.div`
       flex: 0 0 4%;
     }
 
+    &__name {
+      flex: 1 1 auto;
+    }
+
+    &__duration {
+      text-align: right;
+      flex: 0 0 6%;
+      padding-right: 0.5em;
+    }
+
+    &_current {
+      color: ${props => props.theme.primary};
+    }
+
     &:hover {
       background-color: ${props => props.theme.controlBackground};
 
@@ -136,16 +150,6 @@ const ArtistContainer = styled.div`
           display: block;
         }
       }
-    }
-
-    &__name {
-      flex: 1 1 auto;
-    }
-
-    &__duration {
-      text-align: right;
-      flex: 0 0 6%;
-      padding-right: 0.5em;
     }
   }
 
@@ -213,7 +217,7 @@ class Artist extends Component {
       return;
     }
 
-    // booting playlist
+    // booting playlist...
     if (this.state.current === null) {
       const flattenSongs = flatten(this.state.artist.albums.map(album => album.songs));
 
@@ -226,6 +230,7 @@ class Artist extends Component {
         },
       });
 
+      this.setState(() => ({ albumPlayingIndex: -1 }));
       // resuming / pausing playlist
     } else if (this.state.current !== null) {
       store.dispatch({
@@ -235,13 +240,50 @@ class Artist extends Component {
   }
 
   togglePlayPauseAlbum(album, albumIndex) {
-    console.log('album', album, albumIndex);
+    if (this.state.current === null || this.state.albumPlayingIndex !== albumIndex) {
+      store.dispatch({
+        type: PLAY,
+        payload: {
+          play: album.songs[0],
+          queue: album.songs,
+          initialQueue: album.songs,
+        },
+      });
+
+      this.setState(() => ({ albumPlayingIndex: albumIndex }));
+      return;
+    }
+
+    if (this.state.albumPlayingIndex === albumIndex) {
+      store.dispatch({
+        type: TOGGLE_PLAY_PAUSE,
+      });
+    }
   }
 
   togglePlayPauseSong(songId) {
-    console.log('play', songId);
-  }
+    const flattenSongs = flatten(this.state.artist.albums.map(album => album.songs));
+    const songIndex = flattenSongs.findIndex(song => song.songId === songId);
 
+    if (this.state.current !== null && this.state.current.songId === songId) {
+      store.dispatch({
+        type: TOGGLE_PLAY_PAUSE,
+      });
+
+      return;
+    }
+
+    store.dispatch({
+      type: PLAY,
+      payload: {
+        play: flattenSongs[songIndex],
+        queue: flattenSongs,
+        initialQueue: flattenSongs,
+      },
+    });
+
+    this.setState(() => ({ albumPlayingIndex: -1 }));
+  }
 
   render() {
     if (this.state.artist === null) {
@@ -279,7 +321,7 @@ class Artist extends Component {
                 <div className="album__song-list song-list">
                   {
                     album.songs.map((song, songIndex) => (
-                      <div key={song.songId} onDoubleClick={() => this.togglePlayPauseSong(song.songId)} className="song-list__song song">
+                      <div key={song.songId} onDoubleClick={() => this.togglePlayPauseSong(song.songId)} className={`song-list__song song ${this.state.current !== null && song.songId === this.state.current.songId ? 'song_current' : ''}`}>
                         <div className="song__track-number-icon track-number-icon">
                           <div className="track-number-icon__number">{ songIndex + 1 }</div>
                           <i className={`track-number-icon__icon icon-ion-ios-${this.state.current !== null && this.state.current.songId === song.songId && this.state.playing ? 'pause' : 'play'}`} onClick={() => this.togglePlayPauseSong(song.songId)} />

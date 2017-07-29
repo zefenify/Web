a7890674d7f1fc4d38a6d8ca8b384492278c39ac
Eyassu <utopiaio@yahoo.com>
2017-07-29 10:47:00 +0300

    squad 🙌🏿

--- app/component/presentational/Setting.jsx
+++ app/component/presentational/Setting.jsx
@@ -22,6 +22,43 @@ const SettingWrapper = styled.div`
     flex-direction: column;
     align-items: center;
   }
+
+  .utopia {
+    display: flex;
+    flex-direction: column;
+
+    &__squad {
+      margin-top: 8em;
+      margin-bottom: 2em;
+    }
+  }
+
+  .squad {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+
+    a {
+      text-decoration: none;
+    }
+
+    &__howler {
+      background: url(/static/image/howler.svg) no-repeat;
+      display: inline-block;
+      width: 36px;
+      height: 40px;
+    }
+
+    &__plus {
+      font-size: 2em;
+      padding: 0 1em;
+    }
+
+    &__react {
+      font-size: 2.5em;
+    }
+  }
 `;
 
 const Setting = ({
@@ -55,6 +92,14 @@ const Setting = ({
           max="12"
           step="1"
         />
+      </div>
+
+      <div className="utopia">
+        <div className="utopia__squad squad">
+          <a className="squad__howler" href="https://howlerjs.com" rel="noopener noreferrer" target="_blank" />
+          <i className="squad__plus icon-ion-ios-plus-empty" />
+          <a className="squad__react" href="https://facebook.github.io/react" rel="noopener noreferrer" target="_blank"><i className="icon-react" /></a>
+        </div>
       </div>
     </SettingWrapper>
   );

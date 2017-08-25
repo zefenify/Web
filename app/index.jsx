5808b910aa90e180a9b636db5507e357bbb5d214
Eyassu <utopiaio@yahoo.com>
2017-08-25 13:26:34 +0300

    Search SVG

--- app/index.jsx
+++ app/index.jsx
@@ -34,6 +34,22 @@ import { WolfColaContainer, NavListContainer, NavContainer, RouteContainer } fro
 import Divider from '@app/component/styled/Divider';
 import { NavLinkStyled } from '@app/component/styled/ReactRouter';
 
+const Search = props => (
+  <svg
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <circle cx="10.5" cy="10.5" r="7.5" />
+    <line x1="21" y1="21" x2="15.8" y2="15.8" />
+  </svg>
+);
 const WolfCola = DJKhaled('loading', 'theme')(({ loading, theme }) => (
   <Provider store={store}>
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
@@ -50,7 +66,7 @@ const WolfCola = DJKhaled('loading', 'theme')(({ loading, theme }) => (
               <div className="nav-list">
                 <NavLinkStyled to="/search">
                   <span>Search</span>
-                  <i className="icon-ion-ios-search-strong" style={{ float: 'right' }} />
+                  <Search style={{ float: 'right' }} />
                 </NavLinkStyled>
                 <NavLinkStyled to="/top">Top Songs</NavLinkStyled>
                 <NavLinkStyled to="/genre">Genre</NavLinkStyled>

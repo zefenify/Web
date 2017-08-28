bf51218da0b74165945597c31e2a4409e4560451
Eyassu <utopiaio@yahoo.com>
2017-08-28 05:23:18 +0300

    8

--- app/util/randomParam.js
+++ app/util/randomParam.js
@@ -3,7 +3,7 @@ import random from 'lodash/random';
 const seed = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 const seedLength = seed.length - 1;
 
-module.exports = (length = 16) => {
+module.exports = (length = 8) => {
   let randomParam = '';
 
   for (let i = 0; i < length; i += 1) {

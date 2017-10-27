a319d87470134d487042288731ba7c0a4d132f8d
Eyassu <utopiaio@yahoo.com>
2017-10-27 11:03:35 +0300

    ally - no more div's with `onClick`

---
+++ app/component/styled/ClearButton.jsx
@@ -0,0 +1,14 @@
+import styled from 'react-emotion';
+
+const ClearButton = styled.button`
+  display: block;
+  padding: 0;
+  margin: 0;
+  background-color: transparent;
+  text-align: left;
+  color: inherit;
+  border: none;
+  border-radius: 0;
+`;
+
+module.exports = ClearButton;

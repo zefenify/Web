e3bf1011453866c6d4b4b2f7fa61b0f6b2d95dd0
Eyassu <utopiaio@yahoo.com>
2017-10-29 15:36:08 +0300

    Share: Song

--- app/component/container/ContextMenuContainer.jsx
+++ app/component/container/ContextMenuContainer.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 
 import store from '@app/redux/store';
 import { SET_CONTEXT_MENU_OFF } from '@app/redux/constant/contextMenu';
+import { SET_SONG_SAVE, SET_SONG_REMOVE } from '@app/redux/constant/song';
 
 import DJKhaled from '@app/component/hoc/DJKhaled';
 import ContextMenu from '@app/component/presentational/ContextMenu';
@@ -18,6 +20,25 @@ const closeContextMenu = () => {
   });
 };
 
-const ContextMenuContainer = () => <ContextMenu closeContextMenu={closeContextMenu} />;
+const songSave = (track) => {
+  store.dispatch({
+    type: SET_SONG_SAVE,
+    payload: track,
+  });
+};
 
-module.exports = DJKhaled()(ContextMenuContainer);
+const songRemove = (track) => {
+  store.dispatch({
+    type: SET_SONG_REMOVE,
+    payload: track,
+  });
+};
+
+const ContextMenuContainer = props => (<ContextMenu
+  {...props}
+  closeContextMenu={closeContextMenu}
+  songSave={songSave}
+  songRemove={songRemove}
+/>);
+
+module.exports = withRouter(DJKhaled('contextMenu', 'user', 'song')(ContextMenuContainer));
--- app/component/presentational/ContextMenu.jsx
+++ app/component/presentational/ContextMenu.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { func } from 'prop-types';
+import { func, shape } from 'prop-types';
 import styled from 'react-emotion';
 
+import { CONTEXT_SONG, CONTEXT_ALBUM, CONTEXT_ARTIST, CONTEXT_PLAYLIST, CONTEXT_UPLAYLIST } from '@app/redux/constant/contextMenu';
+import { BASE_S3 } from '@app/config/api';
+
 import { CloseSVG } from '@app/component/presentational/SVG';
+import ClearButton from '@app/component/styled/ClearButton';
 
 const ContextMenuContainer = styled.div`
   position: fixed;
@@ -13,7 +17,7 @@ const ContextMenuContainer = styled.div`
   z-index: 999;
   overflow-y: scroll;
   background-color: ${props => props.theme.navbarBackground};
-  color: ${props => props.theme.navbarText};
+  color: ${props => props.theme.navbarTextActive};
   box-shadow: -2px 0 2px 0 ${props => props.theme.navBarBoxShadow};
   transform: translateX(264px);
   transition: transform 250ms;
@@ -23,19 +27,167 @@ const ContextMenuContainer = styled.div`
   }
 
   display: flex;
-  padding: 1em;
+  padding: 1em 0;
+  padding-top: 64px;
   flex-direction: column;
 
   .close-SVG-container {
     position: absolute;
-    left: 1em;
-    top: 1em;
+    top: 0;
+    right: 0;
+    left: 0;
+    padding-left: 1em;
+    padding-top: 1em;
+  }
+
+  .link {
+    flex: 0 0 auto;
+    text-decoration: none;
+    padding: 1em;
+    color: ${props => props.theme.navbarText};
+
+    &:not([disabled]):hover {
+      background-color: ${props => props.theme.controlBackground};
+      color: ${props => props.theme.navbarTextActive};
+    }
+
+    &[disabled] {
+      color: ${props => props.theme.listDividerText};
+    }
+  }
+
+  .song {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 1em;
+    padding: 0 1em;
+    flex: 0 0 auto;
+
+    & > * {
+      margin: 0;
+    }
+
+    &__album-image {
+      width: 164px;
+      height: 164px;
+      border-radius: 6px;
+      border: 1px solid ${props => props.theme.listDivider};
+      margin-bottom: 0.75em;
+    }
+
+    &__name {
+      font-size: 1.25em;
+      margin-bottom: 0.25em;
+    }
+
+    &__album {
+      color: ${props => props.theme.navbarText};
+    }
+  }
+
+  .h-line {
+    flex: 0 0 auto;
+    display: flex;
+    align-items: center;
+    padding-left: 1em;
+    font-size: 0.8em;
+    color: ${props => props.theme.listDividerText};
+
+    span {
+      content: '';
+      flex: 1 1 auto;
+      border-top: 1px solid ${props => props.theme.listDivider};
+    }
   }
 `;
 
+const ContextMenu = ({
+  contextMenu,
+  user,
+  song,
+  closeContextMenu,
+  history,
+  songSave,
+  songRemove,
+}) => {
+  if (contextMenu === null) {
+    return (
+      <ContextMenuContainer id="context-menu-container">
+        <ClearButton className="close-SVG-container" onClick={closeContextMenu}>
+          <CloseSVG />
+        </ClearButton>
+      </ContextMenuContainer>
+    );
+  }
+
+  const { type, payload } = contextMenu;
+  let songSaved = false;
+
+  if (song !== null) {
+    const savedTracks = song.data.song_track;
+    const trackId = payload.track_id;
+    songSaved = savedTracks.includes(trackId);
+  }
+
+  switch (type) {
+    case CONTEXT_SONG:
+      return (
+        <ContextMenuContainer id="context-menu-container">
+          <ClearButton className="close-SVG-container" onClick={closeContextMenu}>
+            <CloseSVG />
+          </ClearButton>
+
+          <div className="song">
+            <div className="song__album-image" style={{ background: `transparent url('${BASE_S3}${payload.track_album.album_cover.s3_name}') 50% 50% / cover no-repeat` }} />
+            <p className="song__name">{ payload.track_name }</p>
+            <p className="song__album">{ payload.track_album.album_name }</p>
+          </div>
+
+          <div className="h-line">Artist&nbsp;<span /></div>
+          <div className="artist">
+            { payload.track_album.album_artist.map(artist => (<ClearButton className="link" disabled={`/artist/${artist.artist_id}` === history.location.pathname} onClick={() => { closeContextMenu(); history.push(`/artist/${artist.artist_id}`); }}>{ artist.artist_name }</ClearButton>)) }
+            { payload.track_featuring.map(artist => (<ClearButton className="link" disabled={`/artist/${artist.artist_id}` === history.location.pathname} onClick={() => { closeContextMenu(); history.push(`/artist/${artist.artist_id}`); }}>{ artist.artist_name }</ClearButton>)) }
+          </div>
+
+          <div className="h-line">Album&nbsp;<span /></div>
+          <ClearButton className="link" disabled={`/album/${payload.track_album.album_id}` === history.location.pathname} onClick={() => { closeContextMenu(); history.push(`/album/${payload.track_album.album_id}`); }}>Go to Album</ClearButton>
+
+          <div className="h-line">Your Library&nbsp;<span /></div>
+          {
+            songSaved
+              ? <ClearButton className="link" disabled={user === null} onClick={() => { closeContextMenu(); songRemove(payload); }}>Remove from Your Library</ClearButton>
+              : <ClearButton className="link" disabled={user === null} onClick={() => { closeContextMenu(); songSave(payload); }}>Save to Your Library</ClearButton>
+          }
+
+          <div className="h-line">Share&nbsp;<span /></div>
+          <a onClick={closeContextMenu} href={`https://www.facebook.com/sharer.php?u=https://play.zefenify.com/album/${payload.track_album.album_id}/${payload.track_id}`} className="link" target="_blank">Facebook</a>
+          <a onClick={closeContextMenu} href={`https://twitter.com/intent/tweet?url=https://play.zefenify.com/album/${payload.track_album.album_id}/${payload.track_id}&text=${payload.track_name}`} className="link" target="_blank">Twitter</a>
+          <a onClick={closeContextMenu} href={`https://telegram.me/share/url?url=https://play.zefenify.com/album/${payload.track_album.album_id}/${payload.track_id}&text=${payload.track_name}`} className="link" target="_blank">Telegram</a>
+        </ContextMenuContainer>
+      );
+
+    default:
+      return null;
+  }
+};
 
 ContextMenu.propTypes = {
+  contextMenu: shape({}),
+  user: shape({}),
+  song: shape({}),
+  history: shape({}),
   closeContextMenu: func.isRequired,
+  songSave: func.isRequired,
+  songRemove: func.isRequired,
+};
+
+ContextMenu.defaultProps = {
+  contextMenu: null,
+  user: null,
+  song: null,
+  history: null,
 };
 
 module.exports = ContextMenu;

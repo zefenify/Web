896df168d0722d52d96717a694b7e9c69368f07f
Eyassu <utopiaio@yahoo.com>
2017-10-31 09:26:30 +0300

    <FixedHeaderList /> for consistency

---
+++ app/component/styled/FixedHeaderList.jsx
@@ -0,0 +1,33 @@
+import styled from 'react-emotion';
+
+const FixedHeaderList = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 0 0 auto;
+  ovefflow-y: auto;
+
+  .title {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    box-shadow: 0 0 4px ${props => props.theme.listBoxShadow};
+    height: 60px;
+    padding: 0 2em;
+  }
+
+  .list {
+    position: absolute;
+    top: 60px;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    overflow-y: auto;
+    padding: 1em 1em;
+  }
+`;
+
+module.exports = FixedHeaderList;

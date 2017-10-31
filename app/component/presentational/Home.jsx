50badefb1189b8bfa3d8711794835b150f993b7c
Eyassu <utopiaio@yahoo.com>
2017-10-31 09:26:57 +0300

    Now using <FixedHeaderList />

--- app/component/presentational/Home.jsx
+++ app/component/presentational/Home.jsx
@@ -1,41 +1,11 @@
 import React from 'react';
 import { func, number, arrayOf, shape } from 'prop-types';
-import styled from 'react-emotion';
 
 import Playlist from '@app/component/presentational/Playlist';
-
-const HomeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 0 0 auto;
-  ovefflow-y: auto;
-
-  .title {
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    box-shadow: 0 0 4px ${props => props.theme.listBoxShadow};
-    height: 60px;
-    padding: 0 2em;
-  }
-
-  .list {
-    position: absolute;
-    top: 60px;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    overflow-y: auto;
-    padding: 1em 1em;
-  }
-`;
+import FixedHeaderList from '@app/component/styled/FixedHeaderList';
 
 const Home = ({ featured, featuredPlayingId, playFeatured }) => (
-  <HomeContainer>
+  <FixedHeaderList>
     <div className="title">
       <h2>Featured</h2>
     </div>
@@ -57,7 +27,7 @@ const Home = ({ featured, featuredPlayingId, playFeatured }) => (
         ))
       }
     </div>
-  </HomeContainer>
+  </FixedHeaderList>
 );
 
 Home.propTypes = {

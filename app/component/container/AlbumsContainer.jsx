0edf10f82298b096297d53e38b007e9b30eb7c09
Eyassu <utopiaio@yahoo.com>
2017-11-06 17:46:49 +0300

    albumsBuild selector

--- app/component/container/AlbumsContainer.jsx
+++ app/component/container/AlbumsContainer.jsx
@@ -1,41 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bool, shape, arrayOf, oneOfType, string } from 'prop-types';
-import cloneDeep from 'lodash/cloneDeep';
 
 import { CONTEXT_MENU_ON_REQUEST, CONTEXT_SONG, CONTEXT_ALBUM } from '@app/redux/constant/contextMenu';
 import { PLAY_REQUEST, PLAY_PAUSE_REQUEST } from '@app/redux/constant/wolfCola';
 
 import store from '@app/redux/store';
-import track from '@app/util/track';
-import sameSongList from '@app/util/sameSongList';
-import { human } from '@app/util/time';
+import albumsBuild from '@app/redux/selector/albumsBuild';
 
 import Albums from '@app/component/presentational/Albums';
 
 class AlbumsContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      albums: [],
-      albumsPlayingId: '',
-      albumPlaying: false,
-      duration: {
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-      },
-    };
+    this.state = albumsBuild(props);
 
     this.albumPlayPause = this.albumPlayPause.bind(this);
     this.albumsPlayPause = this.albumsPlayPause.bind(this);
     this.songPlayPause = this.songPlayPause.bind(this);
     this.contextMenuAlbum = this.contextMenuAlbum.bind(this);
     this.contextMenuSong = this.contextMenuSong.bind(this);
-  }
-
-  componentDidMount() {
-    this.buildAlbums(this.props);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -48,62 +32,7 @@ class AlbumsContainer extends Component {
       return;
     }
 
-    this.buildAlbums(nextProps);
-  }
-
-  buildAlbums(props) {
-    console.log('build albums...');
-
-    if (props.user === null || props.song === null) {
-      this.setState(() => ({
-        albums: [],
-        albumsPlayingId: '',
-      }));
-
-      return;
-    }
-
-    if (Object.hasOwnProperty.call(props.song.included, 'album') === false) {
-      this.setState(() => ({ albums: [] }));
-      return;
-    }
-
-    const included = cloneDeep(props.song.included);
-    const albums = props.match.params.id === undefined ? Object.values(included.album) : Object.values(included.album).filter(album => album.album_id === props.match.params.id);
-    const savedTrackIds = props.song.data.song_track;
-    let albumsPlayingId = '';
-
-    // fix your face, I'm going to be mutating albums...
-    // removing tracks that are not saved in album relationship...
-    albums.forEach((album) => {
-      // albums -> removing tracks that are not saved -> passing to track for referencing...
-      // eslint-disable-next-line
-      album.relationships.track = track(album.relationships.track.filter(trackId => savedTrackIds.includes(trackId)).map(trackId => included.track[trackId]), included);
-      // album artist mapping...
-      // eslint-disable-next-line
-      album.album_artist = album.album_artist.map(artistId => included.artist[artistId]);
-      // eslint-disable-next-line
-      album.album_cover = included.s3[album.album_cover];
-    });
-
-    if (props.match.params.id === undefined && this.props.queueInitial.length > 0) {
-      // finding `albumsPlayingIndex`...
-      albums.forEach((album) => {
-        if (sameSongList(album.relationships.track, this.props.queueInitial) === true) {
-          albumsPlayingId = album.album_id;
-        }
-      });
-    }
-
-    this.setState(() => ({ albums, albumsPlayingId }));
-
-    if (albums.length === 1 && props.match.params.id !== undefined) {
-      // building `duration` and `albumPlaying` for album view...
-      this.setState(() => ({
-        duration: human(albums[0].relationships.track.reduce((totalD, t) => totalD + t.track_track.s3_meta.duration, 0), true),
-        albumPlaying: sameSongList(albums[0].relationships.track, this.props.queueInitial),
-      }));
-    }
+    this.setState(() => albumsBuild(nextProps));
   }
 
   albumPlayPause(albumId) {
@@ -133,7 +62,6 @@ class AlbumsContainer extends Component {
     this.setState(() => ({ albumsPlayingId: albumId }));
   }
 
-  // [x]
   albumsPlayPause() {
     if (this.state.albums === null) {
       return;
@@ -215,20 +143,19 @@ class AlbumsContainer extends Component {
       },
     });
   }
-  // [x]
 
   render() {
     return (
       <Albums
         user={this.props.user}
-        albums={this.state.albums}
         playing={this.props.playing}
-        albumPlayPause={this.albumPlayPause}
-        albumsPlayingId={this.state.albumsPlayingId}
         current={this.props.current}
         albumId={this.props.match.params.id}
+        albums={this.state.albums}
         albumPlaying={this.state.albumPlaying}
         duration={this.state.duration}
+        albumsPlayingId={this.state.albumsPlayingId}
+        albumPlayPause={this.albumPlayPause}
         albumsPlayPause={this.albumsPlayPause}
         songPlayPause={this.songPlayPause}
         contextMenuAlbum={this.contextMenuAlbum}
@@ -246,7 +173,6 @@ AlbumsContainer.propTypes = {
       id: oneOfType([shape({}), string]),
     }),
   }),
-  queueInitial: arrayOf(shape({})),
   user: shape({}),
   song: shape({}),
 };
@@ -255,7 +181,6 @@ AlbumsContainer.defaultProps = {
   playing: false,
   current: null,
   match: { params: { id: undefined } },
-  queueInitial: [],
   user: null,
   song: null,
 };
---
+++ app/redux/selector/albumsBuild.js
@@ -0,0 +1,83 @@
+import { createSelector } from 'reselect';
+import cloneDeep from 'lodash/cloneDeep';
+
+import { human } from '@app/util/time';
+import sameSongList from '@app/util/sameSongList';
+import track from '@app/util/track';
+
+// eslint-disable-next-line
+module.exports = createSelector([props => props.song, props => props.user, props => props.queueInitial, props => props.match.params.id], (song, user, queueInitial, id) => {
+  if (user === null || song === null) {
+    return {
+      albums: [],
+      albumsPlayingId: '',
+      albumPlaying: false,
+      duration: {
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      },
+    };
+  }
+
+  if (Object.hasOwnProperty.call(song.included, 'album') === false) {
+    return {
+      albums: [],
+      albumsPlayingId: '',
+      albumPlaying: false,
+      duration: {
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      },
+    };
+  }
+
+  const included = cloneDeep(song.included);
+  const albums = id === undefined ? Object.values(included.album) : Object.values(included.album).filter(album => album.album_id === id);
+  const savedTrackIds = song.data.song_track;
+  let albumsPlayingId = '';
+
+  // fix your face, I'm going to be mutating albums...
+  // removing tracks that are not saved in album relationship...
+  albums.forEach((album) => {
+    // albums -> removing tracks that are not saved -> passing to track for referencing...
+    // eslint-disable-next-line
+    album.relationships.track = track(album.relationships.track.filter(trackId => savedTrackIds.includes(trackId)).map(trackId => included.track[trackId]), included);
+    // album artist mapping...
+    // eslint-disable-next-line
+    album.album_artist = album.album_artist.map(artistId => included.artist[artistId]);
+    // eslint-disable-next-line
+    album.album_cover = included.s3[album.album_cover];
+  });
+
+  if (id === undefined && queueInitial.length > 0) {
+    // finding `albumsPlayingIndex`...
+    albums.forEach((album) => {
+      if (sameSongList(album.relationships.track, queueInitial) === true) {
+        albumsPlayingId = album.album_id;
+      }
+    });
+  }
+
+  if (albums.length === 1 && id !== undefined) {
+    // building `duration` and `albumPlaying` for album view...
+    return {
+      albums,
+      albumsPlayingId,
+      duration: human(albums[0].relationships.track.reduce((totalD, t) => totalD + t.track_track.s3_meta.duration, 0), true),
+      albumPlaying: sameSongList(albums[0].relationships.track, queueInitial),
+    };
+  }
+
+  return {
+    albums,
+    albumsPlayingId,
+    albumPlaying: false,
+    duration: {
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    },
+  };
+});

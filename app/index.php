a9370d83c1590013d620dba5254cbb3b2241da0d
Eyassu <utopiaio@yahoo.com>
2017-11-11 13:14:03 +0300

    GA

--- app/index.php
+++ app/index.php
@@ -55,5 +55,14 @@
 
   <body>
     <div id="wolf-cola"></div>
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109514993-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-109514993-1');
+    </script>
   </body>
 </html>

d40ad1827496a829a5b8aac3a3a337e7e238f7c3
Eyassu <utopiaio@yahoo.com>
2017-11-19 07:48:08 +0300

    debounce - no need to reinvent the wheel
    - press-n-hold triggers default scroll

--- app/component/container/SpaceContainer.jsx
+++ app/component/container/SpaceContainer.jsx
@@ -2,34 +2,31 @@
 /* eslint no-console: off */
 
 import { Component } from 'react';
+import debounce from 'lodash/debounce';
 
 import { PLAY_PAUSE_REQUEST } from '@app/redux/constant/wolfCola';
 import store from '@app/redux/store';
 
 // we're leaving Redux Saga `throttle` effects - RAW
-// we need to prevent scrolling on space and fire `PLAY_PAUSE_REQUEST` after `KEYPRESS_THROTTLE`
-// so debouching isn't what we're looking for...
-const KEYPRESS_THROTTLE = 128;
-let keypress = null;
+// we need to prevent scrolling on space and fire `PLAY_PAUSE_REQUEST` after `KEY`s
+const KEY = 'keydown';
+const WAIT = 64;
 
 class SpaceContainer extends Component {
   componentDidMount() {
-    const body = document.querySelector('body');
-    body.addEventListener('keydown', (e) => {
-      // shortcutting for faster execution...I think
+    document.querySelector('body').addEventListener(KEY, debounce((e) => {
       if (e.key !== ' ' || window.location.pathname.includes('/search') === true) {
         return;
       }
 
       e.preventDefault();
-      clearTimeout(keypress);
-      keypress = setTimeout(() => {
-        // pre-checks are done inside the saga
-        store.dispatch({
-          type: PLAY_PAUSE_REQUEST,
-        });
-      }, KEYPRESS_THROTTLE);
-    }, { passive: false });
+      store.dispatch({
+        type: PLAY_PAUSE_REQUEST,
+      });
+    }, WAIT, {
+      leading: true,
+      trailing: false,
+    }), { passive: false });
   }
 
   shouldComponentUpdate() {

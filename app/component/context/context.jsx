4c2bf883f9daf0ee4f23c459baec8a0de3195dbf
Eyassu <utopiaio@yahoo.com>
2018-05-11 10:38:17 +0300

    react-redux > 16.3 context API + react-fast-compare
    TODO:
    - ANC the `shouldComponentUpdate` requirement

--- app/component/context/context.jsx
+++ app/component/context/context.jsx
@@ -1,31 +1,83 @@
-import React, { createContext } from 'react';
+import React, { Component, createContext } from 'react';
+import { arrayOf, string, shape, func } from 'prop-types';
+import isEqual from 'react-fast-compare';
 
 const { Provider, Consumer } = createContext();
 
-/**
- * context Consumer HOC
- *
- * @param  {Component} WrappedComponent
- * @param  {...[String]} contextKeys property keys to selectively pass as props
- * @return {Component}
- */
-const withContext = (WrappedComponent, ...contextKeys) => props => (
-  <Consumer>
-    {
-      (context) => {
-        // passes the entire `context` keys as props
-        if (contextKeys.length === 0) {
-          return (<WrappedComponent {...props} context={context} />);
-        }
+class Store extends Component {
+  constructor(props) {
+    super(props);
 
-        // building context keys...
-        const propsKeys = {};
-        contextKeys.forEach((key) => {
-          propsKeys[key] = context[key];
+    if (props.contextKeys.length === 0) {
+      this.state = props.context.getState();
+    } else {
+      const storeSubset = {};
+      const store = props.context.getState();
+
+      props.contextKeys.forEach((key) => {
+        storeSubset[key] = store[key];
+      });
+
+      this.state = storeSubset;
+    }
+  }
+
+  componentDidMount() {
+    this.unsubscribe = this.props.context.subscribe(() => {
+      if (this.props.contextKeys.length === 0) {
+        this.setState(() => this.props.context.getState());
+      } else {
+        const storeSubset = {};
+        const store = this.props.context.getState();
+
+        this.props.contextKeys.forEach((key) => {
+          storeSubset[key] = store[key];
         });
 
-        return (<WrappedComponent {...props} {...propsKeys} />);
+        this.setState(() => storeSubset);
       }
+    });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // TODO:
+    // ANC why without this it'll go into infinite loop
+    return isEqual(this.props, nextProps) === false || isEqual(this.state, nextState) === false;
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    return (<this.props.wrappedComponent {...this.state} {...this.props.wrappedProps} />);
+  }
+}
+
+Store.propTypes = {
+  context: shape({}),
+  contextKeys: arrayOf(string),
+  wrappedComponent: func.isRequired,
+  wrappedProps: shape({}),
+};
+
+Store.defaultProps = {
+  context: null,
+  contextKeys: [],
+  wrappedProps: null,
+};
+
+const withContext = (...contextKeys) => WrappedComponent => props => (
+  <Consumer>
+    {
+      context => (
+        <Store
+          context={context}
+          contextKeys={contextKeys}
+          wrappedComponent={WrappedComponent}
+          wrappedProps={props}
+        />
+      )
     }
   </Consumer>
 );

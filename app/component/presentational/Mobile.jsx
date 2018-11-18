5571c7bacb690716c7943d62f40cce1ce86719ce
Eyassu <utopiaio@yahoo.com>
2018-11-18 16:38:25 +0100

    theme association removed from <Mobile />

--- app/component/presentational/Mobile.jsx
+++ app/component/presentational/Mobile.jsx
@@ -14,8 +14,7 @@ const MobileContainer = styled.div`
   bottom: 0;
   left: 0;
   z-index: 1000;
-  background-color: ${props => props.theme.background};
-  color: ${props => props.theme.text};
+  background-color: hsl(0, 0%, 100%);
 
   @media(min-width: 1024px) {
     display: none;
@@ -24,6 +23,7 @@ const MobileContainer = styled.div`
   .sorry {
     font-size: 2em;
     margin: 0;
+    color: hsl(0, 0%, 30%);
   }
 
   .message {
@@ -34,24 +34,30 @@ const MobileContainer = styled.div`
     padding: 0 1em;
     text-align: center;
     line-height: 1.5em;
+    color: hsl(0, 0%, 30%);
   }
 
   .download {
     font-weight: bold;
     font-size: 1.25em;
     text-decoration: none;
-    color: ${props => props.theme.primary};
+    color: hsl(0, 0%, 30%);
   }
 `;
 
 const Mobile = () => (
   <MobileContainer>
     <p className="sorry">ይቅርታ</p>
-    <p className="message">Zefenify Web is designed for Desktop use.<br />While we are working on iOS and Android apps, you can get the desktop app. Avaialbe for macOS and Windows.</p>
+    <p className="message">
+      <span>Zefenify Web Is Designed for Desktop Use.</span>
+      <br />
+      <span>While We Are Working on iOS and Android Apps, You Can Get the Desktop App. Avaialbe for macOS.</span>
+    </p>
     <a className="download" href="https://github.com/Zefenify/Wolf-Cola/releases" target="_blank" rel="noopener noreferrer">
-      <Download /><span>&nbsp;Get Desktop App</span>
+      <Download />
+      <span>&nbsp;Get Desktop App</span>
     </a>
   </MobileContainer>
 );
 
-module.exports = Mobile;
+export default Mobile;

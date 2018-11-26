2f47eb00c0b8c491fae55829eca188f31defcf50
Eyassu <utopiaio@yahoo.com>
2018-11-26 06:25:43 +0100

    trending is hooked
    - I'm getting used to this hook thing...it's BLACK

--- app/component/container/TrendingContainer.jsx
+++ app/component/container/TrendingContainer.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { string, bool, shape } from 'prop-types';
+import React, { useState, useEffect, useContext } from 'react';
+import { string, shape } from 'prop-types';
 
 import { BASE } from '@app/config/api';
 import { CONTEXT_MENU_ON_REQUEST, CONTEXT_TRACK } from '@app/redux/constant/contextMenu';
@@ -7,110 +7,93 @@ import { PLAY_REQUEST, PLAY_PAUSE_REQUEST } from '@app/redux/constant/wolfCola';
 import trackListSame from '@app/util/trackListSame';
 import track from '@app/util/track';
 import { human } from '@app/util/time';
-
 import api, { error } from '@app/util/api';
-import { loading } from '@app/redux/action/loading';
 import store from '@app/redux/store';
+import { loading } from '@app/redux/action/loading';
 import { urlCurrentPlaying } from '@app/redux/action/urlCurrentPlaying';
-
 import Trending from '@app/component/presentational/Trending';
-import { withContext } from '@app/component/context/context';
+import { Context } from '@app/component/context/context';
 
-class TrendingContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      trending: null,
-      duration: {
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-      },
-      category: props.match.params.category,
-      trendingPlaying: false,
-    };
 
-    this.trendingPlayPause = this.trendingPlayPause.bind(this);
-    this.trackPlayPause = this.trackPlayPause.bind(this);
-    this.contextMenuTrack = this.contextMenuTrack.bind(this);
-  }
+let requestCancel = () => {};
 
-  componentDidMount() {
-    this.loadTracks(this.props.match.params.category);
-  }
+const TrendingContainer = ({ match }) => {
+  const {
+    user,
+    current,
+    playing,
+    queueInitial,
+  } = useContext(Context);
+  const [state, setState] = useState({
+    trending: null,
+    duration: {
+      hour: 0,
+      minute: 0,
+      second: 0,
+    },
+    category: match.params.category || '',
+    trendingPlaying: false,
+  });
 
-  componentDidUpdate(previousProps, previousState) {
-    if (previousState.category !== this.state.category) {
-      this.loadTracks(this.state.category);
-    }
-  }
-
-  componentWillUnmount() {
-    store.dispatch(loading(false));
-    this.cancelRequest();
-  }
-
-  loadTracks(filter) {
-    // this makes sure tab navigation clears previous render
-    this.setState(() => ({
+  useEffect(() => {
+    setState(Object.assign(state, {
       trending: null,
     }));
 
-    if (this.cancelRequest !== undefined) {
-      this.cancelRequest();
-    }
-
     store.dispatch(loading(true));
-    api(`${BASE}trending/${filter}`, this.props.user, (cancel) => {
-      this.cancelRequest = cancel;
-    }, filter === 'today')
-      .then((data) => {
+    api(`${BASE}trending/${match.params.category}`, user, (cancel) => {
+      requestCancel = cancel;
+    }, match.params.category === 'today')
+      .then(({ data, included }) => {
         store.dispatch(loading(false));
 
-        const trending = track(data.data, data.included);
+        const trending = track(data, included);
 
-        this.setState(() => ({
+        setState(Object.assign(state, {
           trending,
-          duration: human(trending.reduce((totalDuration, t) => totalDuration + t.track_track.s3_meta.duration, 0), true),
-        }), () => {
-          const { queueInitial } = store.getState();
+          duration: human(trending.reduce((totalDuration, _track) => totalDuration + _track.track_track.s3_meta.duration, 0), true),
+        }));
 
-          if (queueInitial.length === 0 || this.state.trending.length === 0) {
-            this.setState(() => ({
-              trendingPlaying: false,
-            }));
-
-            return;
-          }
-
-          this.setState(() => ({
-            trendingPlaying: trackListSame(this.state.trending, queueInitial),
+        if (queueInitial.length === 0 || state.trending.length === 0) {
+          setState(Object.assign(state, {
+            trendingPlaying: false,
           }));
-        });
-      }, error(store));
-  }
 
-  trendingPlayPause() {
-    if (this.state.trending === null || this.state.trending.length === 0) {
+          return;
+        }
+
+        setState(Object.assign(state, {
+          trendingPlaying: trackListSame(state.trending, queueInitial),
+        }));
+      }, error(store));
+
+    return () => {
+      store.dispatch(loading(false));
+      requestCancel();
+    };
+  }, [match.params.category]);
+
+  const trendingPlayPause = () => {
+    if (state.trending === null || state.trending.length === 0) {
       return;
     }
 
     // booting playlist...
-    if (this.props.current === null || this.state.trendingPlaying === false) {
+    if (current === null || state.trendingPlaying === false) {
       store.dispatch({
         type: PLAY_REQUEST,
         payload: {
-          play: this.state.trending[0],
-          queue: this.state.trending,
-          queueInitial: this.state.trending,
+          play: state.trending[0],
+          queue: state.trending,
+          queueInitial: state.trending,
         },
       });
 
-      this.setState(() => ({
+      setState(Object.assign(state, {
         trendingPlaying: true,
       }));
 
-      store.dispatch(urlCurrentPlaying(this.props.match.url));
+      store.dispatch(urlCurrentPlaying(match.url));
 
       return;
     }
@@ -118,10 +101,10 @@ class TrendingContainer extends Component {
     store.dispatch({
       type: PLAY_PAUSE_REQUEST,
     });
-  }
+  };
 
-  trackPlayPause(trackId) {
-    if (this.props.current !== null && this.props.current.track_id === trackId) {
+  const trackPlayPause = (trackId = 'ZEFENIFY') => {
+    if (current !== null && current.track_id === trackId) {
       store.dispatch({
         type: PLAY_PAUSE_REQUEST,
       });
@@ -129,7 +112,7 @@ class TrendingContainer extends Component {
       return;
     }
 
-    const trackIndex = this.state.trending.findIndex(t => t.track_id === trackId);
+    const trackIndex = state.trending.findIndex(_track => _track.track_id === trackId);
 
     if (trackIndex === -1) {
       return;
@@ -138,25 +121,25 @@ class TrendingContainer extends Component {
     store.dispatch({
       type: PLAY_REQUEST,
       payload: {
-        play: this.state.trending[trackIndex],
-        queue: this.state.trending,
-        queueInitial: this.state.trending,
+        play: state.trending[trackIndex],
+        queue: state.trending,
+        queueInitial: state.trending,
       },
     });
 
-    this.setState(() => ({
+    setState(Object.assign(state, {
       trendingPlaying: true,
     }));
 
-    store.dispatch(urlCurrentPlaying(this.props.match.url));
-  }
+    store.dispatch(urlCurrentPlaying(match.url));
+  };
 
-  contextMenuTrack(trackId) {
-    if (this.state.trending === null) {
+  const contextMenuTrack = (trackId = 'ZEFENIFY') => {
+    if (state.trending === null) {
       return;
     }
 
-    const trackIndex = this.state.trending.findIndex(t => t.track_id === trackId);
+    const trackIndex = state.trending.findIndex(_track => _track.track_id === trackId);
 
     if (trackIndex === -1) {
       return;
@@ -166,55 +149,33 @@ class TrendingContainer extends Component {
       type: CONTEXT_MENU_ON_REQUEST,
       payload: {
         type: CONTEXT_TRACK,
-        payload: this.state.trending[trackIndex],
+        payload: state.trending[trackIndex],
       },
     });
-  }
-
-  render() {
-    return (
-      <Trending
-        current={this.props.current}
-        playing={this.props.playing}
-        category={this.props.match.params.category}
-        trending={this.state.trending}
-        duration={this.state.duration}
-        trendingPlaying={this.state.trendingPlaying}
-        trendingPlayPause={this.trendingPlayPause}
-        trackPlayPause={this.trackPlayPause}
-        contextMenuTrack={this.contextMenuTrack}
-      />
-    );
-  }
-}
-
-TrendingContainer.getDerivedStateFromProps = (nextProps, prevState) => {
-  if (nextProps.match.params.category === prevState.category) {
-    return null;
-  }
-
-  return {
-    category: nextProps.match.params.category,
   };
+
+  return (
+    <Trending
+      current={current}
+      playing={playing}
+      category={match.params.category}
+      trending={state.trending}
+      duration={state.duration}
+      trendingPlaying={state.trendingPlaying}
+      trendingPlayPause={trendingPlayPause}
+      trackPlayPause={trackPlayPause}
+      contextMenuTrack={contextMenuTrack}
+    />
+  );
 };
 
 TrendingContainer.propTypes = {
-  playing: bool,
-  current: shape({}),
   match: shape({
     url: string,
     params: shape({
       category: string,
     }),
   }).isRequired,
-  user: shape({}),
 };
 
-TrendingContainer.defaultProps = {
-  playing: false,
-  current: null,
-  user: null,
-};
-
-// export default withContext('user', 'current', 'playing')(TrendingContainer);
 export default TrendingContainer;
--- app/component/presentational/Trending.jsx
+++ app/component/presentational/Trending.jsx
@@ -1,83 +1,55 @@
 import React from 'react';
-import { arrayOf, shape, func, number, bool, string } from 'prop-types';
+import {
+  arrayOf,
+  shape,
+  func,
+  number,
+  bool,
+  string,
+} from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import styled from 'react-emotion';
 
 import Track from '@app/component/presentational/Track';
-import Divider from '@app/component/styled/Divider';
 import Button from '@app/component/styled/Button';
 
 const TrendingContainer = styled.div`
-  .header {
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    height: 60px;
-    padding: 0 1em;
-    box-shadow: 0 0 4px 2px ${props => props.theme.navBoxShadow__color};
-  }
-
-  .list {
-    position: absolute;
-    top: 60px;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    overflow-y: auto;
-    padding: 2em;
-    padding-bottom: 0;
-  }
-`;
-
-const TrendingListContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 0 0 auto;
-
-  &.center-content {
-    flex: 1 0 auto;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .trending {
-    display: flex;
-    flex-direction: column;
-    flex: 0 0 auto;
-    margin-bottom: 1em;
-
-    &__title {
-      margin: 0;
-      margin-bottom: -0.15em;
+  .TrendingContainer {
+    &__header {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+      position: absolute;
+      top: 0;
+      right: 0;
+      left: 0;
+      height: 60px;
+      padding: 0 2em;
+      box-shadow: 0 0 4px 2px ${props => props.theme.SHADOW};
     }
 
-    &__info {
-      color: ${props => props.theme.mute};
+    &__view {
+      position: absolute;
+      top: 60px;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      overflow-y: auto;
+      padding: 1rem 2rem 0 2rem;
+      padding-bottom: 0;
     }
 
-    &__button {
-      width: 175px;
-      margin-bottom: 1em;
-    }
-  }
-
-  .track {
-    flex: 1 1 auto;
-
-    & > *:last-child {
-      margin-bottom: 1px;
+    &__duration {
+      color: ${props => props.theme.NATURAL_4};
     }
   }
 `;
+
 
 const NavLinkStyled = styled(NavLink)`
-  color: ${props => props.theme.navLink__color};
-  padding: 0 1em;
-  font-size: 1.2em;
+  color: ${props => props.theme.NATURAL_2};
+  padding: 0 1rem;
+  font-size: 1.2rem;
   font-weight: bold;
   text-decoration: none;
   border-bottom: 2px solid transparent;
@@ -87,15 +59,16 @@ const NavLinkStyled = styled(NavLink)`
   justify-content: center;
 
   &:hover {
-    color: ${props => props.theme.navLink__color_hover};
+    color: ${props => props.theme.NATURAL_2};
   }
 
   &.active {
-    color: ${props => props.theme.navLinkActive__color};
-    border-bottom: 2px solid ${props => props.theme.primary};
-    background-color: ${props => props.theme.navLinkActive__backgroundColor};
+    color: ${props => props.theme.PRIMARY_4};
+    border-bottom: 2px solid ${props => props.theme.PRIMARY_4};
+    background-color: ${props => props.theme.NATURAL_7};
   }
 `;
+
 
 const Trending = ({
   trending,
@@ -112,7 +85,7 @@ const Trending = ({
     // repeating block to avoid the ? : in the _actual_ render
     return (
       <TrendingContainer>
-        <div className="header">
+        <div className="TrendingContainer__header">
           <NavLinkStyled to="/trending/today">Today</NavLinkStyled>
           <NavLinkStyled to="/trending/yesterday">Yesterday</NavLinkStyled>
           <NavLinkStyled to="/trending/week">Week</NavLinkStyled>
@@ -122,49 +95,43 @@ const Trending = ({
     );
   }
 
-  const { hours, minutes, seconds } = duration;
+  const { hour, minute, second } = duration;
   // eslint-disable-next-line
   const trendingTitle = category === 'yesterday' ? 'Trending Yesterday' : category === 'today' ? 'Trending Today' : category === 'week' ? 'Trending This Week' : 'Popular Tracks of All Time';
 
   return (
     <TrendingContainer>
-      <div className="header">
+      <div className="TrendingContainer__header">
         <NavLinkStyled to="/trending/today">Today</NavLinkStyled>
         <NavLinkStyled to="/trending/yesterday">Yesterday</NavLinkStyled>
         <NavLinkStyled to="/trending/week">Week</NavLinkStyled>
         <NavLinkStyled to="/trending/popularity">All Time</NavLinkStyled>
       </div>
 
-      <div className="list">
-        <TrendingListContainer>
-          <div className="trending">
-            <h1 className="trending__title">{ trendingTitle }</h1>
-            <p className="trending__info">{`${trending.length} song${trending.length > 1 ? 's' : ''}, ${hours > 0 ? `${hours} hr` : ''} ${minutes} min ${hours > 0 ? '' : `${seconds} sec`}`}</p>
-            <Button className="trending__button" onClick={trendingPlayPause}>{`${(playing && trendingPlaying) ? 'PAUSE' : 'PLAY'}`}</Button>
-          </div>
+      <div className="TrendingContainer__view">
+        <div className="d-flex flex-column align-items-start mb-3">
+          <h1 className="m-0 p-0 mt-3">{ trendingTitle }</h1>
+          <p className="m-0 p-0 mt-2 TrendingContainer__duration">{`${trending.length} song${trending.length > 1 ? 's' : ''}, ${hour > 0 ? `${hour} hr` : ''} ${minute} min ${hour > 0 ? '' : `${second} sec`}`}</p>
+          <Button className="mt-2" style={{ width: '125px' }} onClick={trendingPlayPause}>{`${(playing && trendingPlaying) ? 'PAUSE' : 'PLAY'}`}</Button>
+        </div>
 
-          <Divider />
-
-          <div className="track">
-            {
-              trending.map((track, index) => (
-                <Track
-                  key={track.track_id}
-                  currentTrackId={current === null ? '' : current.track_id}
-                  trackNumber={index + 1}
-                  trackPlayPause={trackPlayPause}
-                  playing={playing}
-                  trackId={track.track_id}
-                  trackName={track.track_name}
-                  trackFeaturing={track.track_featuring}
-                  trackDuration={track.track_track.s3_meta.duration}
-                  trackAlbum={track.track_album}
-                  contextMenuTrack={contextMenuTrack}
-                />
-              ))
-            }
-          </div>
-        </TrendingListContainer>
+        {
+          trending.map((track, index) => (
+            <Track
+              key={track.track_id}
+              currentTrackId={current === null ? '' : current.track_id}
+              trackNumber={index + 1}
+              trackPlayPause={trackPlayPause}
+              playing={playing}
+              trackId={track.track_id}
+              trackName={track.track_name}
+              trackFeaturing={track.track_featuring}
+              trackDuration={track.track_track.s3_meta.duration}
+              trackAlbum={track.track_album}
+              contextMenuTrack={contextMenuTrack}
+            />
+          ))
+        }
       </div>
     </TrendingContainer>
   );
@@ -176,9 +143,9 @@ Trending.propTypes = {
   playing: bool,
   category: string,
   duration: shape({
-    hours: number,
-    minutes: number,
-    seconds: number,
+    hour: number,
+    minute: number,
+    second: number,
   }),
   trendingPlaying: bool,
   trendingPlayPause: func.isRequired,
@@ -192,9 +159,9 @@ Trending.defaultProps = {
   current: null,
   playing: false,
   duration: {
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
+    hour: 0,
+    minute: 0,
+    second: 0,
   },
   trendingPlaying: false,
 };

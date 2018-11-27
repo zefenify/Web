02079ca2cebe591c48efa7e2a2e8310005bd86fb
Eyassu <utopiaio@yahoo.com>
2018-11-27 05:30:12 +0100

    what the flip Qhue

--- app/component/container/QueueContainer.jsx
+++ app/component/container/QueueContainer.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { bool, shape, arrayOf, string } from 'prop-types';
+import React, { useContext } from 'react';
+import { shape, string } from 'prop-types';
 
 import { PLAY_REQUEST, PLAY_PAUSE_REQUEST } from '@app/redux/constant/wolfCola';
 import { CONTEXT_MENU_ON_REQUEST, CONTEXT_TRACK } from '@app/redux/constant/contextMenu';
@@ -8,45 +8,33 @@ import store from '@app/redux/store';
 import tracksDuration from '@app/redux/selector/tracksDuration';
 import { urlCurrentPlaying } from '@app/redux/action/urlCurrentPlaying';
 
-import Tracks from '@app/component/presentational/Tracks';
-import { withContext } from '@app/component/context/context';
+import Queue from '@app/component/presentational/Queue';
+import { Context } from '@app/component/context/context';
 
-class QueueContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      durationTotal: tracksDuration({ tracks: props.queueNext }),
-    };
+const QueueContainer = ({ match }) => {
+  const {
+    playing,
+    current,
+    queueNext,
+    queueInitial,
+  } = useContext(Context);
 
-    this.queueNextPlayPause = this.queueNextPlayPause.bind(this);
-    this.trackPlayPause = this.trackPlayPause.bind(this);
-    this.contextMenuTrack = this.contextMenuTrack.bind(this);
-  }
-
-  queueNextPlayPause() {
-    if (this.state.queueNextPlaying === true) {
-      store.dispatch({
-        type: PLAY_PAUSE_REQUEST,
-      });
-
-      return;
-    }
-
+  const queueNextPlay = () => {
     store.dispatch({
       type: PLAY_REQUEST,
       payload: {
-        play: this.props.queueNext[0],
-        queue: this.props.queueNext,
-        queueInitial: this.props.queueInitial,
+        play: queueNext[0],
+        queue: queueNext,
+        queueInitial,
       },
     });
 
-    store.dispatch(urlCurrentPlaying(this.props.match.url));
-  }
+    store.dispatch(urlCurrentPlaying(match.url));
+  };
 
-  trackPlayPause(trackId) {
-    if (this.props.current === null || this.props.current.track_id !== trackId) {
-      const trackIndex = this.props.queueNext.findIndex(track => track.track_id === trackId);
+  const trackPlayPause = (trackId = 'ZEFENIFY') => {
+    if (current === null || current.track_id !== trackId) {
+      const trackIndex = queueNext.findIndex(track => track.track_id === trackId);
 
       if (trackIndex === -1) {
         return;
@@ -55,23 +43,23 @@ class QueueContainer extends Component {
       store.dispatch({
         type: PLAY_REQUEST,
         payload: {
-          play: this.props.queueNext[trackIndex],
-          queue: this.props.queueNext,
-          queueInitial: this.props.queueInitial,
+          play: queueNext[trackIndex],
+          queue: queueNext,
+          queueInitial,
         },
       });
 
-      store.dispatch(urlCurrentPlaying(this.props.match.url));
+      store.dispatch(urlCurrentPlaying(match.url));
       return;
     }
 
     store.dispatch({
       type: PLAY_PAUSE_REQUEST,
     });
-  }
+  };
 
-  contextMenuTrack(trackId) {
-    const trackIndex = this.props.queueNext.findIndex(track => track.track_id === trackId);
+  const contextMenuTrack = (trackId = 'ZEFENIFY') => {
+    const trackIndex = queueNext.findIndex(track => track.track_id === trackId);
 
     if (trackIndex === -1) {
       return;
@@ -81,50 +69,28 @@ class QueueContainer extends Component {
       type: CONTEXT_MENU_ON_REQUEST,
       payload: {
         type: CONTEXT_TRACK,
-        payload: this.props.queueNext[trackIndex],
+        payload: queueNext[trackIndex],
       },
     });
-  }
+  };
 
-  render() {
-    return (
-      <Tracks
-        titleMain="Queue"
-        titleEmpty="Queue is empty...spice it up እንጂ"
-        playing={this.props.playing}
-        current={this.props.current}
-        tracks={this.props.queueNext}
-        durationTotal={this.state.durationTotal}
-        tracksPlaying={false}
-        tracksPlayPauseButtonShow={false}
-        tracksPlayPause={this.queueNextPlayPause}
-        trackPlayPause={this.trackPlayPause}
-        contextMenuTrack={this.contextMenuTrack}
-      />
-    );
-  }
-}
-
-QueueContainer.getDerivedStateFromProps = nextProps => ({
-  durationTotal: tracksDuration({ tracks: nextProps.queueNext }),
-});
+  return (
+    <Queue
+      playing={playing}
+      current={current}
+      queueNext={queueNext}
+      durationTotal={tracksDuration({ tracks: queueNext })}
+      queueNextPlay={queueNextPlay}
+      trackPlayPause={trackPlayPause}
+      contextMenuTrack={contextMenuTrack}
+    />
+  );
+};
 
 QueueContainer.propTypes = {
-  playing: bool,
-  current: shape({}),
-  queueNext: arrayOf(shape({})),
-  queueInitial: arrayOf(shape({})),
   match: shape({
     url: string,
   }).isRequired,
 };
 
-QueueContainer.defaultProps = {
-  playing: false,
-  current: null,
-  queueNext: [],
-  queueInitial: [],
-};
-
-// export default withContext('playing', 'current', 'queueNext', 'queueInitial', 'totalDuration')(QueueContainer);
 export default QueueContainer;
---
+++ app/component/presentational/Queue.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import {
+  func,
+  shape,
+  bool,
+  number,
+  arrayOf,
+} from 'prop-types';
+
+import Track from '@app/component/presentational/Track';
+import Button from '@app/component/styled/Button';
+import HeaderView from '@app/component/styled/HeaderView';
+
+const Queue = ({
+  playing,
+  current,
+  queueNext,
+  durationTotal,
+  queueNextPlay,
+  trackPlayPause,
+  contextMenuTrack,
+}) => {
+  if (queueNext.length === 0) {
+    return (
+      <div className="d-flex flex-column flex-grow-1 flex-shrink-0 align-items-center justify-content-center">
+        <h2 className="mute">Queue Is Empty...Spice It up እንጂ</h2>
+      </div>
+    );
+  }
+
+  const { hour, minute, second } = durationTotal;
+
+  return (
+    <HeaderView>
+      <div className="__header">
+        <h1>Queue</h1>
+      </div>
+
+      <div className="__view">
+        <div className="d-flex flex-column align-items-start flex-grow-1 px-3">
+          <p className="m-0 p-0 mt-3">{`${queueNext.length} Song${queueNext.length > 1 ? 's' : ''} • ${hour > 0 ? `${hour} hr` : ''} ${minute} min ${hour > 0 ? '' : `${second} sec`}`}</p>
+          <Button className="mt-2 mb-3" style={{ width: '125px' }} onClick={queueNextPlay}>PLAY</Button>
+
+          {
+            queueNext.map((track, index) => (
+              <Track
+                key={track.track_id}
+                currentTrackId={current === null ? '' : current.track_id}
+                trackNumber={index + 1}
+                trackPlayPause={trackPlayPause}
+                playing={playing}
+                trackId={track.track_id}
+                trackName={track.track_name}
+                trackFeaturing={track.track_featuring}
+                trackDuration={track.track_track.s3_meta.duration}
+                trackAlbum={track.track_album}
+                contextMenuTrack={contextMenuTrack}
+              />
+            ))
+          }
+        </div>
+      </div>
+    </HeaderView>
+  );
+};
+
+Queue.propTypes = {
+  playing: bool,
+  current: shape({}),
+  queueNext: arrayOf(shape({})),
+  durationTotal: shape({
+    hour: number,
+    minute: number,
+    second: number,
+  }),
+  queueNextPlay: func.isRequired,
+  trackPlayPause: func.isRequired,
+  contextMenuTrack: func.isRequired,
+};
+
+Queue.defaultProps = {
+  playing: false,
+  current: null,
+  queueNext: [],
+  durationTotal: {
+    hour: 0,
+    minute: 0,
+    second: 0,
+  },
+};
+
+export default Queue;

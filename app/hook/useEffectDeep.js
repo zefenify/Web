8f5290678f7a6debb975eb16ff026f00b0d45aed
Eyassu <utopiaio@icloud.com>
2019-03-09 12:34:41 +0100

    POC on useDeepEffect

--- app/hook/useEffectDeep.js
+++ app/hook/useEffectDeep.js
@@ -2,38 +2,36 @@ import { useEffect, useRef } from 'react';
 import isEqual from 'react-fast-compare';
 
 
-// const useEffectDeep = (callback, input) => {
-//   const previousInputRef = useRef(null);
-//   const cleanupRef = useRef();
-//   const inputAreEqual = isEqual(previousInputRef.current, input);
-
-//   useEffect(() => {
-//     // ANC:
-//     // this seems to be blocking initial render calls...any change on `inputsAreEqual`
-//     // should be calling `callback()` right?
-//     if (inputAreEqual === false) {
-//       cleanupRef.current = callback(); // assigning callback's return i.e. the cleanup function
-//     }
-
-//     return cleanupRef.current;
-//   }, [inputAreEqual]);
-
-//   useEffect(() => {
-//     previousInputRef.current = input;
-//   });
-// };
-
-
 const useEffectDeep = (callback, input) => {
   const previousInputRef = useRef(null);
-  const inputAreEqual = isEqual(previousInputRef.current, input);
+  const previousEquality = useRef(null);
 
-  useEffect(() => callback(), [inputAreEqual]);
+  const previousAndCurrentInputAreEqual = isEqual(previousInputRef.current, input);
+  const useEffectDependency = previousEquality.current === previousAndCurrentInputAreEqual
+    ? previousEquality.current
+    : previousAndCurrentInputAreEqual;
+
+  useEffect(() => {
+    previousEquality.current = previousAndCurrentInputAreEqual;
+    return callback();
+  }, [useEffectDependency]);
 
   useEffect(() => {
     previousInputRef.current = input;
   });
 };
+
+
+// const useEffectDeep = (callback, input) => {
+//   const previousInputRef = useRef(null);
+//   const inputAreEqual = isEqual(previousInputRef.current, input);
+
+//   useEffect(() => callback(), [inputAreEqual]);
+
+//   useEffect(() => {
+//     previousInputRef.current = input;
+//   });
+// };
 
 
 export default useEffectDeep;

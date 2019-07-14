a3e73571ea50f535389d54732697ad2be1f3b6fe
Eyassu <utopiaio@icloud.com>
2019-07-14 07:54:18 +0200

    all dem packages are now updated

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {

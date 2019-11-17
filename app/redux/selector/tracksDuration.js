15561f0bbcd0ffb0d756f4e5fd31cdbfbf1acea1
Eyassu <utopiaio@icloud.com>
2019-11-17 14:08:47 +0100

    task duration selector G-eed

--- app/redux/selector/tracksDuration.js
+++ app/redux/selector/tracksDuration.js
@@ -2,4 +2,4 @@ import { createSelector } from 'reselect';
 
 import time from '@app/util/time';
 
-export default createSelector([state => state.tracks], tracks => time(tracks.reduce((totalDuration, track) => totalDuration + track.track_track.s3_meta.duration, 0), true));
+export default createSelector([state => state.tracks], tracks => time(tracks.reduce((totalDuration, track) => totalDuration + track.track.meta.duration, 0), true));

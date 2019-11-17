85562dca9b8a586f0c73cc4d5fc95f5287624ef7
Eyassu <utopiaio@icloud.com>
2019-11-17 13:29:28 +0100

    I might get your song played in the radio station

--- app/component/presentational/ArtistList.jsx
+++ app/component/presentational/ArtistList.jsx
@@ -8,8 +8,8 @@ const ArtistList = ({ artist }) => (
   <Fragment>
     {
       artist.map((_artist, index) => (
-        <Link to={`/artist/${_artist.artist_id}`} key={_artist.artist_id}>
-          <span>{_artist.artist_name}</span>{ (artist.length > 1 && (index < artist.length - 1)) ? <span>{ (index < artist.length - 2) ? <span>,</span> : <span>&nbsp;&amp;</span> } </span> : null }
+        <Link to={`/artist/${_artist.id}`} key={_artist.id}>
+          <span>{_artist.name}</span>{ (artist.length > 1 && (index < artist.length - 1)) ? <span>{ (index < artist.length - 2) ? <span>,</span> : <span>&nbsp;&amp;</span> } </span> : null }
         </Link>
       ))
     }

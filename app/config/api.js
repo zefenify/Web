a3a4e7b1c1f6a06e2fcc62a4e2b11e36f87d8d2a
Eyassu <utopiaio@icloud.com>
2019-12-07 04:46:15 +0100

    api pointing to live server

--- app/config/api.js
+++ app/config/api.js
@@ -1,17 +1,17 @@
 // production
-// export const HEADER = 'Authorization';
-// export const BASE = 'https://zefenify.com/api/';
-// export const BASE_S3 = 'https://zefenify.s3.eu-central-1.amazonaws.com/'; // AWS
-// export const BASE_SHARE = 'https://zefenify.com/';
-// export const FAUTH = 'https://zefenify.com/api/fauth/';
-// export const SEARCH = 'https://zefenify.com/api/search';
-// export const CACHE_AGE = 21600; // in seconds (6 hours)
+export const HEADER = 'Authorization';
+export const BASE = 'https://zefenify.com/api/';
+export const BASE_S3 = 'https://zefenify.s3.eu-central-1.amazonaws.com/'; // AWS
+export const BASE_SHARE = 'https://zefenify.com/';
+export const FAUTH = 'https://zefenify.com/api/fauth/';
+export const SEARCH = 'https://zefenify.com/api/search';
+export const CACHE_AGE = 21600; // in seconds (6 hours)
 
 // development
-export const HEADER = 'Authorization';
-export const BASE = 'http://api.zefenify.io/';
-export const BASE_S3 = 'http://api.zefenify.io/@S3/';
-export const BASE_SHARE = 'http://zefenify.io/';
-export const FAUTH = 'http://api.zefenify.io/fauth/';
-export const SEARCH = 'http://api.zefenify.io/search';
-export const CACHE_AGE = 0; // in seconds
+// export const HEADER = 'Authorization';
+// export const BASE = 'http://api.zefenify.io/';
+// export const BASE_S3 = 'http://api.zefenify.io/@S3/';
+// export const BASE_SHARE = 'http://zefenify.io/';
+// export const FAUTH = 'http://api.zefenify.io/fauth/';
+// export const SEARCH = 'http://api.zefenify.io/search';
+// export const CACHE_AGE = 0; // in seconds
